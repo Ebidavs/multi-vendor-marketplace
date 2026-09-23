dbf26b59a7221b96ba9923b87b1070409227f9bf
iamuwaomaroseline-love/silver-pancake <iamuwaomaroseline@gmail.com>
2026-09-23 23:31:28 +0100

    Build frontend home and product pages

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+
+import "./Navbar.css"
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <h2>Marketplace</h2>
+            </div>
+
+            <div className="navbar-links">
+                <a href="/">Home</a>
+                <a href="/products">Products</a>
+                <a href="/login">login</a>
+                <a href="/register">Register</a>
+            </div>
+
+            <div>
+                <button>cart</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ frontend/src/pages/products.jsx
@@ -0,0 +1,26 @@
+import "./products.css"
+
+function Products(){
+    return (
+        
+        <div className="products-page">
+            <h1>Products</h1>
+            <p>Explore products from different vendors.</p>
+        
+            
+                
+                <div className="product-grid">
+
+                <div className="product-card">
+                    <div className="product-image">Electronics</div>
+                </div>
+
+            </div>
+    </div>
+        
+        
+
+        
+    );
+}
+export default Products
