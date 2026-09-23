e124e1d86aca269b3e10200b1a0b1a4d8db930de
iamuwaomaroseline-love/silver-pancake <iamuwaomaroseline@gmail.com>
2026-09-24 00:36:04 +0100

    Add sixth product category

--- frontend/src/pages/products.jsx
+++ frontend/src/pages/products.jsx
@@ -13,8 +13,44 @@ function Products(){
 
                 <div className="product-card">
                     <div className="product-image">Electronics</div>
+                    <h2>Electronics</h2>
+                    <p>Phones, laptops, gadgets and more.</p>
+                    <button>Shop Now</button>
+
+                </div>
+                <div className="product-card">
+                    <div className="product-image">Fashion</div>
+                    <h2>Fashion</h2>
+                    <p>Clothing, shoes, bags and accessories</p>
+                    <button>Shop Now</button>
                 </div>
 
+                <div className="product-card">
+                    <div className="product-image">Makeup & Beauty</div>
+                    <h2>Makeup & Beauty</h2>
+                    <p>Makeup, skincare and beauty products</p>
+                    <button>Shop Now</button>
+                </div>
+            <div className="product-card">
+                <div className="product-image">Home & Kitchen</div>
+                <h2>Home & Kitchen</h2>
+                <p>Furniture, kitchen items and home essentials</p>
+                <button>Shop Now</button>
+            </div>
+
+            <div className="product-card">
+        <div className="product-image">Shoes & Bags</div>
+        <h2>Shoes & Bags</h2>
+        <p>Footwear, handbag, backpacks and more.</p>
+        <button>Shop Now</button>
+            </div>
+
+            <div className="product-card">
+                <div className="product-image">Sports & Fitness</div>
+                <h2>Sports & Fitness</h2>
+                <p>Sportswear, gym equipment and fitness essentials.</p>
+                <button>Shop Now</button>
+            </div>
             </div>
     </div>
         
